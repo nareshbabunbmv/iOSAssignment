855c65d56bf2f9f0d9752af3443c6e128f446cca
nareshbabunbmv <nareshbabunbmv@gmail.com>
2019-12-12 00:05:49 +0530

    Search by city with autosuggestion feature added

--- AirportSearch/AirportSearch/Controller/AirportSearchController.swift
+++ AirportSearch/AirportSearch/Controller/AirportSearchController.swift
@@ -12,6 +12,8 @@ class AirportSearchController: UIViewController,ViewModelDelegate {
 
     var allAirportDetails:[AirportDetailModel] = [AirportDetailModel]()
     @IBOutlet weak var airportListTable: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    var filteredDetails:[AirportDetailModel] = []
 
     override func viewDidLoad() {
         self.airportListTable.isHidden = true
@@ -36,6 +38,8 @@ class AirportSearchController: UIViewController,ViewModelDelegate {
         self.view.activityStopAnimating()
         self.allAirportDetails.removeAll()
         self.allAirportDetails.append(contentsOf: allAirportDetails)
+        self.filteredDetails.append(contentsOf: allAirportDetails)
+
         //load the list once received all the details of the airport
         if self.allAirportDetails.count != 0 {
             self.airportListTable.isHidden = false
@@ -46,18 +50,43 @@ class AirportSearchController: UIViewController,ViewModelDelegate {
 
 extension AirportSearchController : UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.allAirportDetails.count
+        return self.filteredDetails.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "detailCell", for: indexPath) as! AirportDetailCell
 
-        cell.airportCode.text = self.allAirportDetails[indexPath.row].code
-        cell.city.text = self.allAirportDetails[indexPath.row].city
-        cell.country.text = self.allAirportDetails[indexPath.row].country
-        cell.state.text = self.allAirportDetails[indexPath.row].state
+        cell.airportCode.text = self.filteredDetails[indexPath.row].code
+        cell.city.text = self.filteredDetails[indexPath.row].city
+        cell.country.text = self.filteredDetails[indexPath.row].country
+        cell.state.text = self.filteredDetails[indexPath.row].state
         
         return cell
         
+    }
+}
+
+extension AirportSearchController:UISearchBarDelegate {
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        filteredDetails = searchText.isEmpty ? self.allAirportDetails : self.allAirportDetails.filter({ (value) -> Bool in
+            return value.city.range(of: searchText, options: .caseInsensitive) != nil
+        })
+        
+        if filteredDetails.count != 0 {
+            self.airportListTable.isHidden = false
+            self.airportListTable.reloadData()
+        } else {
+            self.airportListTable.isHidden = true
+        }
+        
+        if searchText.count == 0 {
+            searchBar.perform(#selector(resignFirstResponder), with: nil, afterDelay: 0.1)
+        }
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
     }
 }
