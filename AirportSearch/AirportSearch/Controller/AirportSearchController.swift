f868e61e6f36af87d37b853f290ee196e3452769
nareshbabunbmv <nareshbabunbmv@gmail.com>
2019-12-11 20:00:42 +0530

    Creating New Project For iOS Assignment

---
+++ AirportSearch/AirportSearch/Controller/AirportSearchController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AirportSearch
+//
+//  Created by Naresh on 11/12/19.
+//  Copyright © 2019 Naresh. All rights reserved.
+//
+
+import UIKit
+
+class AirportSearchController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
