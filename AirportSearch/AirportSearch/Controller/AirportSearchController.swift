d7d9eac1ad98063f4d3c5bfc7c8da49b6ada11f8
nareshbabunbmv <nareshbabunbmv@gmail.com>
2019-12-11 23:54:08 +0530

    Showing the airport details in the list

--- AirportSearch/AirportSearch/Controller/AirportSearchController.swift
+++ AirportSearch/AirportSearch/Controller/AirportSearchController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class AirportSearchController: UIViewController,ViewModelDelegate {
-    
+
     var allAirportDetails:[AirportDetailModel] = [AirportDetailModel]()
-    
+    @IBOutlet weak var airportListTable: UITableView!
+
     override func viewDidLoad() {
+        self.airportListTable.isHidden = true
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
@@ -31,9 +33,31 @@ class AirportSearchController: UIViewController,ViewModelDelegate {
     }
     
     func load(allAirportDetails: [AirportDetailModel]) {
+        self.view.activityStopAnimating()
         self.allAirportDetails.removeAll()
         self.allAirportDetails.append(contentsOf: allAirportDetails)
         //load the list once received all the details of the airport
+        if self.allAirportDetails.count != 0 {
+            self.airportListTable.isHidden = false
+            self.airportListTable.reloadData()
+        }
     }
 }
 
+extension AirportSearchController : UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.allAirportDetails.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "detailCell", for: indexPath) as! AirportDetailCell
+
+        cell.airportCode.text = self.allAirportDetails[indexPath.row].code
+        cell.city.text = self.allAirportDetails[indexPath.row].city
+        cell.country.text = self.allAirportDetails[indexPath.row].country
+        cell.state.text = self.allAirportDetails[indexPath.row].state
+        
+        return cell
+        
+    }
+}
---
+++ AirportSearch/AirportSearch/CustomView/AirportDetailCell.swift
@@ -0,0 +1,17 @@
+//
+//  AirportDetailCell.swift
+//  AirportSearch
+//
+//  Created by Naresh on 11/12/19.
+//  Copyright © 2019 Naresh. All rights reserved.
+//
+
+import UIKit
+
+class AirportDetailCell: UITableViewCell {
+
+    @IBOutlet weak var airportCode: UILabel!
+    @IBOutlet weak var city: UILabel!
+    @IBOutlet weak var state: UILabel!
+    @IBOutlet weak var country: UILabel!
+}
